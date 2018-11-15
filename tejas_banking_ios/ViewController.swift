c30127a364092d051b09e204d93ed4d63c5eecb6
Tejas Jadhav <tejas@tejass-macbook-pro.local>
2018-11-15 08:44:02 -0500

    Initial Commit

---
+++ tejas_banking_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tejas_banking_ios
+//
+//  Created by Tejas Jadhav on 2018-11-15.
+//  Copyright © 2018 Tejas Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
